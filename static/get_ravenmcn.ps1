5c9a947dcd94e159df2912d1d020bd4652a0852b
KeilAwk <82655578+KeilAwk@users.noreply.github.com>
2024-08-18 12:09:05 +0800

    Upde.

--- static/get_ravenmcn.ps1
+++ static/get_ravenmcn.ps1
@@ -1,7 +1,9 @@
 #RavenMCN安装脚本
 
-Out-String -InputObject "# RavenM国内版安装脚本
-# 由RavenfieldCommunity维护"
+Write-Host "# RavenM国内版安装脚本
+# RavenM国内版由 Ravenfield贴吧 维护
+# 安装脚本由 Github@RavenfieldCommunity 维护
+"
 
 #定义变量
 $isOldFileExist = $false
@@ -9,7 +11,7 @@ $isOldFileExist = $false
 #获取本地路径
 $envVar = Get-ChildItem Env:appdata
 $path = $envVar.Value
-$folderPath = $path + "\RavenmCN"
+$folderPath = $path + "\RavenMCN"
 $zipPath = $folderPath + "\RavenMCN.zip"
 $exePath = $folderPath + "\RavenM一键安装工具.exe"
 
@@ -21,11 +23,11 @@ $isOldFileExist = Test-Path -Path $zipPath
 
 #打印下载目录
 $tipText = "下载目录：" + $folderPath
-Out-String -InputObject $tipText
+Write-Host $tipText
 
 #定义函数
 function Download-RavenMCN {
-  Out-String -InputObject "正在下载文件..." 
+  Write-Host "正在下载文件..." 
   #创建session并使用直链api请求文件
   $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
   $session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0"
@@ -59,25 +61,25 @@ function CheckAndRun-RavenMCN {
   #校验hash
   $hash1 = Get-FileHash $zipPath -Algorithm SHA256
   $hash2 = $hash1.Hash
-  Out-String -InputObject "安装文件Hash: $hash2"
+  Write-Host "安装文件Hash: $hash2"
   if ($hash2 -eq "946539FC1FF3B99D148190AD04435FAF9CBDD7706DBE8159528B91D7ED556F78") 
   { 
     Run-RavenMCN
   }
   else 
   { 
-    Out-String -InputObject "安装文件校验不通过，请反馈给社区管理或重新下载"
+    Write-Host "安装文件校验不通过，请反馈给社区管理或重新下载"
     Read-Host -prompt "您现在可以关闭窗口了" 
   }
 }
 
 function Run-RavenMCN {
   #解压
-  Out-String -InputObject "正在启动文件..."
+  Write-Host "正在启动文件..."
   Expand-Archive $zipPath -DestinationPath $folderPath -Force
   #运行   
   Start-Process $exePath
-  Out-String -InputObject "注意，运行安装文件不需要管理员权限"
+  Write-Host "注意，运行安装文件不需要管理员权限"
   Read-Host -prompt "请手动安装完RavenM后再关闭本窗口"
   Read-Host -prompt "请手动安装完RavenM后再关闭本窗口"
 }
@@ -91,7 +93,7 @@ function MainGet-RavenMCN {
   }
   else
   {
-    Out-String -InputObject "安装文件下载失败，请检查网络或反馈给社区管理"
+    Write-Host "安装文件下载失败，请检查网络或反馈给社区管理"
     Read-Host -prompt "您现在可以关闭窗口了"
   }
 }
@@ -100,9 +102,8 @@ function MainGet-RavenMCN {
 #主代码
 if ($isOldFileExist -eq $true)
 {
-  Out-String -InputObject "本地存在安装文件，是否直接运行？" 
-  $yesRun = Read-Host -Prompt "是请输入'1'，否则重新下载安装文件"
-  Out-String -InputObject "" 
+  Write-Host "本地存在安装文件，是否直接运行？" 
+  $yesRun = Read-Host -Prompt "是请输入'1'并回车，其他任意键并回车则重新下载安装文件"
   if ($yesRun  -eq "1")
   {
     CheckAndRun-RavenMCN
